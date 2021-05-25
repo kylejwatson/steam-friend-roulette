f03cff1565c6838f6e00a99d0fd6e52c36264bc4
Kyle Watson <mail@watsonk.me>
2021-05-25 15:52:08 +0200

    Added prod build commands

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  serverUrl: 'https://api.steam.watsonk.me',
+  steamImageUrl: 'https://media.steampowered.com/steamcommunity/public/images/apps'
 };
